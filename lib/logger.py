ffe6e1caf41ab86859d749cd407b14c09c6d14dc
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-07-07 23:19:47 +0900

    fix: ライブラリを更新

--- lib/logger.py
+++ lib/logger.py
@@ -4,8 +4,12 @@ import coloredlogs
 import logging
 import logging.handlers
 import bz2
+import pathlib
 import os
 import io
+
+MAX_SIZE = 10 * 1024 * 1024
+ROTATE_COUNT = 10
 
 LOG_FORMAT = "{name} %(asctime)s %(levelname)s [%(filename)s:%(lineno)s %(funcName)s] %(message)s"
 
@@ -21,14 +25,37 @@ class GZipRotator:
         os.remove(source)
 
 
-def init(name, is_str=False):
-    coloredlogs.install(fmt=LOG_FORMAT.format(name=name))
+def init(name, level=logging.WARNING, dir_path=None, is_str=False):
+    coloredlogs.install(fmt=LOG_FORMAT.format(name=name), level=level)
+
+    if dir_path is not None:
+        log_path = pathlib.Path(dir_path)
+        log_path.mkdir(exist_ok=True, parents=True)
+
+        log_file_path = str(log_path / "{name}.log".format(name=name))
+
+        logging.info("Log to {log_file_path}".format(log_file_path=log_file_path))
+
+        logger = logging.getLogger()
+        log_handler = logging.handlers.RotatingFileHandler(
+            log_file_path,
+            encoding="utf8",
+            maxBytes=MAX_SIZE,
+            backupCount=ROTATE_COUNT,
+        )
+        log_handler.formatter = logging.Formatter(
+            fmt=LOG_FORMAT.format(name=name), datefmt="%Y-%m-%d %H:%M:%S"
+        )
+        log_handler.namer = GZipRotator.namer
+        log_handler.rotator = GZipRotator.rotator
+
+        logger.addHandler(log_handler)
 
     if is_str:
         str_io = io.StringIO()
         handler = logging.StreamHandler(str_io)
         handler.formatter = logging.Formatter(
-            fmt=LOG_FORMAT, datefmt="%Y-%m-%d %H:%M:%S"
+            fmt=LOG_FORMAT.format(name=name), datefmt="%Y-%m-%d %H:%M:%S"
         )
         logging.getLogger().addHandler(handler)
 
